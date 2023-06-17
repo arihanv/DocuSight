c98691b8e53d056bbbd8ab3ce7426d64d5a1ca08
arihanv <arihanvaranasi@gmail.com>
2023-06-17 13:01:16 -0500

    UI of File

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,8 +8,13 @@ const IndexPage = (props: Props) => {
       <div className="flex flex-col items-center justify-center gap-4 w-full max-w-[950px] m-auto">
         <h1 className="text-5xl font-extrabold leading-tight tracking-tighter md:text-7xl">Dashboard</h1>
         <div className="dark:bg-gray-900 bg-gray-100 w-full max-w-[700px] p-2.5 flex items-center justify-center h-[550px] rounded-xl">
-          <div>
-           Files
+          <div className="bg-background grid p-2.5 rounded-lg w-full max-w-[200px] gap-2">
+            <h1 className="text-lg font-semibold border-b py-1 space-x-0">
+              The way the world works
+            </h1>
+            <div className="text-xs text-muted-foreground">
+              Date uploaded: <span className="italic">10/10/2021</span>
+            </div>
           </div>
         </div>
       </div>
