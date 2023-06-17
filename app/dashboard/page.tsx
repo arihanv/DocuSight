569fc0f83fca2c554a1161eb4ed266824ca58a29
arihanv <arihanvaranasi@gmail.com>
2023-06-17 12:53:02 -0500

    Base UI For Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react"
 
 type Props = {}
 
 const IndexPage = (props: Props) => {
   return (
-    <div>
-        <h1>IndexPage</h1>
-    </div>
+    <section className="container grid items-center gap-6 pb-8 py-5 md:py-8">
+      <div className="flex flex-col items-center justify-center gap-4 w-full max-w-[950px] m-auto">
+        <h1 className="text-5xl font-extrabold leading-tight tracking-tighter md:text-7xl">Dashboard</h1>
+        <div className="dark:bg-gray-900 bg-gray-100 w-full max-w-[700px] p-2.5 flex items-center justify-center h-[550px] rounded-xl">
+          <div>
+           Files
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
